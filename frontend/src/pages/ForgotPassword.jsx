ef8d7187c4e15d0cfbfce2508bea245180507bbe
tushar <tusharthakre800@gmail.com>
2026-02-09 12:50:14 +0530

    vercel code

--- frontend/src/pages/ForgotPassword.jsx
+++ frontend/src/pages/ForgotPassword.jsx
@@ -5,6 +5,7 @@ import ThemeToggle from '../components/ThemeToggle';
 import axios from 'axios';
 import toast from 'react-hot-toast';
 
+
 const ForgotPassword = () => {
   const navigate = useNavigate();
   const [email, setEmail] = useState('');
